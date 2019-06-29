acd268cb11e198d3c536175e3a0d335b8246138f
Juan M. Nava Z <juanmnzsk8@gmail.com>
2019-06-29 12:48:49 -0500

    se integra el gradiente en la imagen general

--- proyecto_curso/lib/review_list.dart
+++ proyecto_curso/lib/review_list.dart
@@ -16,6 +16,20 @@ class ReviewList extends StatelessWidget{
         ),
         Review(
             "assets/img/people.jpg",
+            "David",
+            "1 review . 5 photos",
+            "Es un gran lugar para visitar",
+            4.5
+        ),
+        Review(
+            "assets/img/people.jpg",
+            "David",
+            "1 review . 5 photos",
+            "Es un gran lugar para visitar",
+            4.5
+        ),
+        Review(
+            "assets/img/people.jpg",
             "Fernando",
             "2 review . 7 photos",
             "Excelente para las vacaciones",
